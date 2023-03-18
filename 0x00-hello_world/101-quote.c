feb2b428e7eadd2e9b7e304ed5719e0c87ea60b8
cecilacecsoh <ceciloluoch95@gmail.com>
2023-03-18 12:52:35 -0700

    8. UNIX is basically a simple_ fput

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <inistd.h>
+/**
+ * main - program that print words line
+ * Return: 1 when success
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
