7b5fb81fd73afe9993c4937bb6211c9b359c01b7
cecilacecsoh <ceciloluoch95@gmail.com>
2023-03-18 14:03:35 -0700

    8. UNIX is basically a simple_ write'

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
+
 /**
  * main - program that print words line
  * Return: 1 when success
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
