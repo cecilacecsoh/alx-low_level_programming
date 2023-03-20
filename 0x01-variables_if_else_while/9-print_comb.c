5af3224b967b3a9d2067051e8713b009ca049867
cecilacecsoh <ceciloluoch95@gmail.com>
2023-03-20 14:00:02 -0700

    9.Prints all single-digit numbers with a comma

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,14 +12,13 @@ int main(void)
 
 	for (p = 0 ; p < 10 ; p++)
 	{
-		if (p == 9)
-			putchar(p + '0');
-		else
+		putchar(p + '0');
+		if (p != 9)
 		{
-			putchar(p + '0');
 			putchar(',');
 			putchar(' ');
 		}
 	}
+	putchar('\n');
 	return (0);
 }
