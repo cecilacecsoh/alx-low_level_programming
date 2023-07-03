4fe8b8c55f13e83cea0beb23c240f76dc3953587
cecilacecsoh <ceciloluoch95@gmail.com>
2023-07-03 09:57:26 -0700

    more_singly_linked_lists

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -5,7 +5,7 @@
 
 /**
  * get_nodeint_at_index -  returns the nth node
- * 			of a listint_t linked list.
+ * of a listint_t linked list.
  *
  * @head: first node of listint_t
  * @index: the index of the node, starting at 0
